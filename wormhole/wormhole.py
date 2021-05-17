b70b563ceadb62cd290ccbafb3c0e9309ca1eb52
Grivan Thapar <grivan@thapar.me>
2021-05-16 22:20:23 -0700

    wormhole works

--- wormhole/wormhole.py
+++ wormhole/wormhole.py
@@ -13,55 +13,59 @@ for i in range(N):
   x,y = map(int, fin.readline().split())
   wholes.append((x,y))
 
-x_max = max(list(zip(*wholes))[0])
-# print("x_max = "+str(x_max))
-# print("a=(0,0) b=(1,0) c=(1,1) d=(0,1)")
+partners = [-1 for i in range(N)]
 
-# you have to go into a wormhole to get into a loop
-# so we start with each wormhole and then see if we get
-# stuck in a loop (arrive at the same wormhole again)
-def check(pairs):
-  loop = False
-  for hole in wholes:
-    current = hole
-    seen = set()
-    seen.add(current)
-    while(not loop and current[0] <= x_max):
-      if current in pairs:
-        current = pairs[current]
-        current = (current[0] + 1, current[1])
-      else:
-        current = (current[0] + 1, current[1])
-      if current in seen:
-        # print("deteced!")
-        loop = True
+# nright contains the nearest right for each hole
+nright = [-1 for i in range(N)]
+for i in range(N):
+  for j in range(N):
+    xi, yi = wholes[i]
+    xj, yj = wholes[j]
+    if xj > xi and yi == yj:
+      if nright[i] == -1 or (xj - xi < wholes[nright[i]][0] - xi):
+        nright[i] = j
+
+def check():
+  # print("partners = "+ str(partners))
+  # print("nright = " + str(nright))
+  for i in range(N):
+    current = i
+    # print("current = "+ str(current))
+    for _ in range(N):
+      current = nright[partners[current]]
+      #print("with move "+ str(_) + " current = "+ str(current))
+      if current == -1:
         break
-      else:
-         seen.add(current)
-    if loop: return 1
+    if current != -1:
+      #print("loop! current = " + str(current))
+      return 1
   return 0
 
-total = 0
-def generate(wholes, pairs):
-  global total
-  if len(wholes) == 0:
-    if check(pairs):
-      # print("loop :" + str(pairs))
-      total+=1
-    return
+def solve():
+  total = 0
+  ui = None
+  for i in range(N):
+    if partners[i] == -1:
+      ui = i
+      break
+  else:
+    # if every hole is paried
+    if check(): return 1
+    return 0
+    
+  for j in range(ui+1, N):
+    if partners[j] == -1:
+      partners[i] = j
+      partners[j] = i
+      total += solve()
+      partners[i] = -1
+      partners[j] = -1
+  
+  return total
 
-  hole1 = wholes.pop()
-  for hole in wholes:
-    pairs[hole] = hole1
-    pairs[hole1] = hole
-    nwholes = wholes.copy()
-    nwholes.remove(hole)
-    generate(nwholes, pairs)
-    del pairs[hole]
-    del pairs[hole1]
+# print(nright)
+# print(wholes)
+total = solve()
 
-generate(wholes.copy(), dict())
-# print("check = " + str(check({(1, 0): (0, 1), (0, 0): (1, 1)})))
-# print(total)
 fout.write(str(total) + '\n')
 fout.close()
